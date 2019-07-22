40fbf53f88d2dba2e4251a27e9a4fb4bd9403411
Kishor Naik <kishor.naik011.net@gmail.com>
2019-07-22 15:16:46 +0530

    Code Commit

---
+++ main.js
@@ -0,0 +1,4 @@
+var tuplesObj;
+tuplesObj = ["Hello", 1];
+console.log(tuplesObj[0]);
+console.log(tuplesObj[1]);
---
+++ main.ts
@@ -0,0 +1,5 @@
+let tuplesObj: [string, number];
+tuplesObj = ["Hello", 1];
+
+console.log(tuplesObj[0]);
+console.log(tuplesObj[1]);
